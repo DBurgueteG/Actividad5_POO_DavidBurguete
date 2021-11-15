72f9d917ab8bc001918b19ba9030c2516de291ef
dburguetegdaw1 <dburguetegdaw1@gmail.com>
2021-11-15 19:00:33 +0100

    David Burguete: Creado el archivo Empleado.php y la clase Empleado

---
+++ Empleado.php
@@ -0,0 +1,41 @@
+<?php 
+    class Empleado{
+        private $nombre;
+        private $apellido;
+        private $numeroSeguridadSocial;
+
+        function __construct($nombre, $apellido, $numeroSeguridadSocial){
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+        }
+
+        public function getNombre(){
+                return $this->nombre;
+        }
+
+        public function setNombre($nombre){
+                $this->nombre = $nombre;
+        }
+
+        public function getApellido(){
+                return $this->apellido;
+        }
+
+        public function setApellido($apellido){
+                $this->apellido = $apellido;
+        }
+
+        public function getNumeroSeguridadSocial(){
+                return $this->numeroSeguridadSocial;
+        }
+
+        public function setNumeroSeguridadSocial($numeroSeguridadSocial){
+                $this->numeroSeguridadSocial = $numeroSeguridadSocial;
+        }
+
+        public function mostrar(){
+            return "Esta empleado " . $this->nombre . " " . $this->apellido . " con el NSS: " . $this->numeroSeguridadSocial;
+        }
+    }
+?>
