218ceceb8ccbb783cc4712f9c71d7c6f3c0ebcea
dburguetegdaw1 <dburguetegdaw1@gmail.com>
2021-11-17 18:57:45 +0100

    David Burguete: Añadido vista_resultado.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     include "EmpleadoPorComision.php";
     include "PruebaPolimorf.php";
     include "Empresa.php";
+    
     $empleadoPlantilla = new EmpleadoPlantilla("Jon", "García", "30-38327317-23", 47, 24);
     $empleadoPlantillaAdicional = new EmpleadoPlantilla("David", "Burguete", "83-19385826-73", 173, 201);
     $empleadoPorComision = new EmpleadoPorComision("Iñigo", "Alonso", "29-64191684-26", 15, 94, 6);
@@ -12,16 +13,19 @@
     $empresa->addEmpleado($empleadoPorComision);
     $empresa->addEmpleado($empleadoPlantilla);
 
-    echo "<p><b>Empleado:</b></p><br><br><br>";
-    echo "<p><b>EmpleadoAsalariado:</b></p>";
-    echo $empleadoPlantilla->mostrar();
-    echo $empleadoPlantillaAdicional->mostrar();
-    echo "<br><p><b>EmpleadoPorComision:</b></p>";
-    echo $empleadoPorComision->mostrar();
-    echo $empleadoPorComisionAdicional->mostrar();
-    echo "<br><p><b>Prueba Polimorfismo:</b></p>";
-    echo PruebaPolimorf::calcular($empleadoPlantilla);
-    echo "<br><p><b>Listado Empresa:</b></p>";
-    echo $empresa->listarEmpleados() . "<br><br><br>";
-    echo $empresa->sumaIngresos();
+    $resultado = "";
+    $resultado .= "<p><b>Empleado:</b></p><br><br><br>";
+    $resultado .= "<p><b>EmpleadoAsalariado:</b></p>";
+    $resultado .= $empleadoPlantilla->mostrar();
+    $resultado .= $empleadoPlantillaAdicional->mostrar();
+    $resultado .= "<br><p><b>EmpleadoPorComision:</b></p>";
+    $resultado .= $empleadoPorComision->mostrar();
+    $resultado .= $empleadoPorComisionAdicional->mostrar();
+    $resultado .= "<br><p><b>Prueba Polimorfismo:</b></p>";
+    $resultado .= PruebaPolimorf::calcular($empleadoPlantilla);
+    $resultado .= "<br><p><b>Listado Empresa:</b></p>";
+    $resultado .= $empresa->listarEmpleados() . "<br><br><br>";
+    $resultado .= $empresa->sumaIngresos();
+
+    include "includes/vista_resultado.php";
 ?>
